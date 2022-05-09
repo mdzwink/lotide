1202d1ee6e6c91a90cf8a3b53c907a708a7382e0
mdzwink <mdzwink@gmail.com>
2022-05-09 07:02:53 -0400

    adding assertArrayEqual.js to lotide

---
+++ assertArrayEqual.js
@@ -0,0 +1,24 @@
+const assertArrayEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✔ >ARRAY< Assertion Passed✔: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔ >ARRAY< Assertion Failed⛔: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {//checks if length of arrays are not equal and returns false if not
+    return false;
+  };
+  for (let item in arr1) {//checks if any item in one array is not equal to the item in the second array at the same index
+    // console.log('arr1:', arr1[item], 'arr2:', arr2[item])
+    if (arr1[item] !== arr2[item]) {
+      // console.log('arr1:', arr1[item], 'arr2:', arr2[item])
+      return false;
+    }
+  }
+  return true;
+};
+
+
+assertArrayEqual([1, 2, 3], [1, 2, 3]);
